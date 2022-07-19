882773a317aaca09328b8b2c95a56fda11a61a71
Jason Kundi <jasonskundi@jasons-mbp.home>
2022-07-19 16:01:21 +0100

    Page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { header, btn } from '../styles/home.module.css'
+import { header, btn } from "../styles/home.module.css"
 
 const Home = ({ children }) => {
   return (
     <Layout>
-      <section className={header}>{children}
+      <section className={header}>
+        {children}
         <div>
           <h2>Full-Stack</h2>
           <h3>React, JavaScript, Node.JS</h3>
           <p>Software Developer based in London</p>
-          <Link to="/projects" className={btn}>{children} Check out my work</Link>
+          <Link to="/projects" className={btn}>
+            {children} Check out my work
+          </Link>
         </div>
         <img className="profile-pic" alt="Jason" src="/Jason.png"></img>
       </section>
