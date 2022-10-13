9482590f90775f157c15ef3f2458e3cdc8963a29
Jason Kundi <jasonkundi1996@gmail.com>
2022-10-13 16:36:13 +0100

    Carousel

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,28 +1,42 @@
 import React from "react"
-import { Carousel } from 'react-carousel-minimal';
+import { Carousel } from "react-carousel-minimal"
 import Layout from "../../components/Layout"
 import { portfolio } from "../../styles/projects.module.css"
 
 const Projects = ({ children }) => {
   const data = [
     {
-      image: "https://user-images.githubusercontent.com/94011909/170559949-4c7a122d-b5fc-481d-8f5e-4234988a08fd.PNG",
-      caption: "Hylla - Music app"
-    }
+      image:
+        "https://user-images.githubusercontent.com/94011909/176274187-f0d4c59a-200c-483e-9a3b-c05db894bcf8.PNG",
+      caption: "React Meal Planner",
+    },
+    {
+      image:
+        "https://user-images.githubusercontent.com/94011909/170559949-4c7a122d-b5fc-481d-8f5e-4234988a08fd.PNG",
+      caption: "Hylla - Music app",
+    },
+    {
+      image: "/greengrocers.png",
+      caption: "Grocery E-Store",
+    },
+    {
+      image: "cohort-manager.png",
+      caption: "Cohort Manager",
+    },
   ]
   const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
+    fontSize: "2em",
+    fontWeight: "bold",
   }
   const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
+    fontSize: "20px",
+    fontWeight: "bold",
   }
   return (
     <Layout>
       <div className={portfolio}>
         {children}
-        <h2>My Favourites</h2>
+        <h2> A couple of faves</h2>
         <div
           style={{
             padding: "0 20px",
